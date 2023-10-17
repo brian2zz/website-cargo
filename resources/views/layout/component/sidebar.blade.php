96f2654bac228f1a2184bf8d4995dbea8467b7dd
brian2zz <brianlopez12357@gmail.com>
2023-10-17 20:48:38 +0700

    first commit

---
+++ resources/views/layout/component/sidebar.blade.php
@@ -0,0 +1,30 @@
+<nav id="sidebar" class="sidebar js-sidebar">
+    <div class="sidebar-content js-simplebar">
+        <a class="sidebar-brand" href="index.html">
+            <span class="align-middle">AdminKit</span>
+        </a>
+        <ul class="sidebar-nav">
+            <li class="sidebar-header">
+                Pages
+            </li>
+
+            <li class="sidebar-item active">
+                <a class="sidebar-link" href="{{ url('/') }}">
+                    <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="#">
+                    <i class="align-middle" data-feather="user"></i> <span class="align-middle">Surat Jalan</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="#">
+                    <i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Manifest</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
